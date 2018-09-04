2f68be5b471a4eaa07f0b2e1efe02bb21a5c8453
Jeong <hee2012j@naver.com>
2018-09-04 12:45:13 +0900

    add contine or not

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -4,14 +4,22 @@ public class App {
     public static void main(String[] args) {
      
        Scanner keyIn = new Scanner(System.in);
-       System.out.print("이름? ");
-       String name = keyIn.nextLine();
-       System.out.print("이메일? ");
-       String email = keyIn.nextLine();
-       System.out.print("비밀번호? ");
-       String password = keyIn.nextLine();
        
+       while(true) {
+       System.out.print("name? ");
+       String name = keyIn.nextLine();
+       System.out.print("email? ");
+       String email = keyIn.nextLine();
+       System.out.print("password? ");
+       String password = keyIn.nextLine();
        System.out.printf("%s, %s, %s\n",name,email,password);
-            
+       
+       System.out.println("continue?(Y/n) ");
+       String answer = keyIn.nextLine();
+       if(answer.toLowerCase().equals("n"))
+           break;
+       } 
+       
+       
     }
 }
