da10e7fce3558398c465c1d51fedb67707cbe85c
Jeong <hee2012j@naver.com>
2018-09-04 14:37:48 +0900

    make array to input more information

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -3,16 +3,25 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
      
+        String []names = new String[100];
+        String []emails = new String[100];
+        String []passwords = new String[100];
+        
+        int index =0;
+        
        Scanner keyIn = new Scanner(System.in);
        
        while(true) {
        System.out.print("name? ");
-       String name = keyIn.nextLine();
+       names[index] = keyIn.nextLine();
+       
        System.out.print("email? ");
-       String email = keyIn.nextLine();
+       emails[index] = keyIn.nextLine();
+       
        System.out.print("password? ");
-       String password = keyIn.nextLine();
-       System.out.printf("%s, %s, %s\n",name,email,password);
+       passwords[index] = keyIn.nextLine();
+       
+       index++;
        
        System.out.println("continue?(Y/n) ");
        String answer = keyIn.nextLine();
@@ -20,6 +29,10 @@ public class App {
            break;
        } 
        
-       
+       for(int i=0; i<index; i++) {
+       System.out.printf("%s, %s, %s\n",names[i],emails[i],passwords[i]);
+       //위치 이동할 때, alt 누르고 방향기로 위 아래!
+       }
+       keyIn.close();
     }
 }
