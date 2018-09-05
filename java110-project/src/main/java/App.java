e0f5ed24fb152db3df4f1a958692362631285cac
Jeong <hee2012j@naver.com>
2018-09-05 10:17:17 +0900

    add menu moduel

--- java110-project/src/main/java/App.java
+++ java110-project/src/main/java/App.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 public class App {
-    
+
+
     //여러 속성의 값을 관리하기 쉽도록 사용자 정의 데이터 타입을 만들어 사용함.
     static class Member{
         protected String name;
         protected String emial;
         protected String password;
-        
+
         //인스턴스의 메모리는 다루는 operator=getter/setter=accessor=property=message
         public String getName() {
             return name;
@@ -28,16 +29,69 @@ public class App {
             this.password = password;
         }
     }
-    
+
+      
     static Member [] members = new Member[100];
     static int index =0;
 
     static Scanner keyIn = new Scanner(System.in);
 
     public static void main(String[] args) {
-        inputMembers();
-        printMembers();
+
+        while(true) {
+            String menu=promptMenu();
+            if(menu.equals("1")) {
+                serviceStudentMenu();
+            }
+            else if(menu.equals("0")) {
+                System.out.println("good-bye!");
+                break;
+            }
+        }
         keyIn.close();
+    }
+
+    private static void serviceStudentMenu() {
+        while(true) {
+            System.out.print("student menu> ");
+            String command=keyIn.nextLine();
+            if(command.equals("list")) {
+                printMembers();
+            }
+            else if(command.equals("add")) {
+                inputMembers();
+            }
+            else if(command.equals("quit")) {
+                break;
+            }
+            else {
+                System.out.println("command is not vaild");
+            }
+        }
+    }
+
+    private static String promptMenu() {
+        System.out.println("menu");
+        System.out.println("0. exit");
+        System.out.println("1. student");
+        System.out.println("2. teacher");
+        System.out.println("3. manager");
+
+        while(true) {
+            System.out.print("menu number> ");
+
+            String menu=keyIn.nextLine();
+
+            switch(menu) {
+            case "1":
+            case "2":
+            case "3":
+            case "0":
+                return menu; //break 없어도 가능하네.
+            default:
+                System.out.println("menu number is not vaild ");
+            }
+        }   
     }
 
     static void printMembers() {
@@ -48,7 +102,7 @@ public class App {
             //method 앞에  static 붙이면 classmethod
         }
     }
-    
+
     static void inputMembers() {
         while(true) {
             Member m = new Member();
@@ -58,7 +112,7 @@ public class App {
             m.setEmial(keyIn.nextLine());
             System.out.print("password? ");
             m.setPassword(keyIn.nextLine());
-            
+
             members[index++] = m;
             /*index++; 이걸 안쓰려면 위에 방법처럼 [index ++]*/
 
