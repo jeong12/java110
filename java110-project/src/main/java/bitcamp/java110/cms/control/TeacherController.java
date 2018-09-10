e897ad959b6273728fe6d18df3f0f1c72ea522e6
Jeong <hee2012j@naver.com>
2018-09-10 10:50:02 +0900

    use RequestMapping annotation and remove control interface

--- java110-project/src/main/java/bitcamp/java110/cms/control/TeacherController.java
+++ java110-project/src/main/java/bitcamp/java110/cms/control/TeacherController.java
@@ -4,17 +4,19 @@ import java.util.List;
 import java.util.Scanner;
 
 import bitcamp.java110.cms.annotation.Component;
+import bitcamp.java110.cms.annotation.RequestMapping;
 import bitcamp.java110.cms.domain.Teacher;
 
-@Component("2")
-public class TeacherController implements Controller{
+@Component("teacher")
+public class TeacherController{
 
     private List<Teacher> teachers = new ArrayList<>();
     public TeacherController(){
         init();
     }
 
-      public void service(Scanner keyIn) {
+      @RequestMapping
+      public void teacher(Scanner keyIn) {
         while(true) {
             System.out.print("teacher menu> ");
             String command=keyIn.nextLine();
