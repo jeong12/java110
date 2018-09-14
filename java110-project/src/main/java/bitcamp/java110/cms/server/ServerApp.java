21c2d707bb71c60799e0870f09c4bb74111d94ef
Jeong <hee2012j@naver.com>
2018-09-14 17:06:41 +0900

    process xxxx/list

--- java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
+++ java110-project/src/main/java/bitcamp/java110/cms/server/ServerApp.java
@@ -7,44 +7,95 @@ import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bitcamp.java110.cms.context.RequestMappingHandlerMapping;
+import bitcamp.java110.cms.context.RequestMappingHandlerMapping.RequestMappingHandler;
+
 public class ServerApp {
 
+    ClassPathXmlApplicationContext iocContainer;
+    RequestMappingHandlerMapping requestHandlerMap; 
+    
+    public ServerApp() throws Exception{
+        createIoCContatiner();
+        logBeansOfContatiner();
+        processRequestMappingAnnotation();
+    }
 
-    public static void main(String[] args) throws Exception {
+    private void createIoCContatiner() {
+        iocContainer = new ClassPathXmlApplicationContext(
+                "bitcamp/java110/cms/conf/Application-context.xml");
+    }
+    
+    private void processRequestMappingAnnotation() {
+        requestHandlerMap = new RequestMappingHandlerMapping();
+        String[] names = iocContainer.getBeanDefinitionNames();
+        for (String name : names) {
+            Object obj = iocContainer.getBean(name);            
+            requestHandlerMap.addMapping(obj);
+        }
+    }
 
+    private void logBeansOfContatiner() {
+        System.out.println(":::::::::::::::::::::");
+        String[]nameList=iocContainer.getBeanDefinitionNames();
+        for(String name:nameList) {
+            System.out.println(name);
+        }
+        System.out.println(":::::::::::::::::::::");
+    }
+
+    public void service() throws Exception{
         //클라이언트 연결을 기다리는 서버 소켓 준비      포트번호,backlog(대기열 크기) : 대기열은 que방식!
         ServerSocket seversocket = new ServerSocket(8888);
         System.out.println("서버 실행 중...");
 
         while(true) {
             try(
-                Socket socket = seversocket.accept();
-                PrintStream out=new PrintStream(
-                                new BufferedOutputStream(
+                    Socket socket = seversocket.accept();
+                    PrintStream out=new PrintStream(
+                            new BufferedOutputStream(
                                     socket.getOutputStream()));
 
-                BufferedReader in = new BufferedReader(
-                                    new InputStreamReader(
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(
                                     socket.getInputStream()));
-                ){
+                    ){
                 System.out.println(in.readLine());
-                out.println("OK"); out.flush();
+                out.println("OK:희정"); out.flush();
+                
                 while(true) {
-                String requestLine = in.readLine();
-                if(requestLine.equals("EXIT")) {
-                    out.println("goodbye");
-                    out.println(); out.flush();
-                    break;
+                    String requestLine = in.readLine();
+                    if(requestLine.equals("EXIT")) {
+                        out.println("goodbye");
+                        out.println(); out.flush();
+                        break;
+                    }
+                    RequestMappingHandler mapping = 
+                            requestHandlerMap.getMapping(requestLine);
+                    if (mapping == null) {
+                        out.println("해당 요청을 처리할 수 없습니다.");
+                        out.println(); out.flush();
+                        continue;
+                    }
+                    try {
+                    mapping.getMethod().invoke(mapping.getInstance(), out);
+                    }catch(Exception e) {
+                        e.printStackTrace();
+                        out.println("요청 처리 중에 오류가 발생했습니다.");
+                    }
+                    out.println();
+                    out.flush();
                 }
-                out.println(requestLine);
-                out.println();
-                out.flush();
             }
-
-
-
         }
-
     }
-}
+
+    
+    public static void main(String[] args) throws Exception {
+
+        ServerApp serverApp = new ServerApp();
+        serverApp.service();
+    }
 }
