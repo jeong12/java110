6d96f8ba30c4f5e754d4374d1f854e8420b39dcd
Jeong <hee2012j@naver.com>
2018-10-19 16:34:07 +0900

    Mybatis와 Spring IoC 연결

--- java110-project/src/main/java/bitcamp/java110/cms/service/impl/AuthServiceImpl.java
+++ java110-project/src/main/java/bitcamp/java110/cms/service/impl/AuthServiceImpl.java
@@ -2,8 +2,6 @@ package bitcamp.java110.cms.service.impl;
 
 import java.util.HashMap;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,28 +14,25 @@ import bitcamp.java110.cms.service.AuthService;
 @Service
 public class AuthServiceImpl implements AuthService {
 
-    @Autowired
-    SqlSessionFactory sqlSessionFactory;
+    @Autowired ManagerDao managerDao;
+    @Autowired TeacherDao teacherDao;
+    @Autowired StudentDao studentDao;
 
     @Override
     public Member getMember(
             String email, String password, String memberType) {
         
-        try(SqlSession session = sqlSessionFactory.openSession()){
         HashMap<String, Object> params = new HashMap<>();
         params.put("email", email);
         params.put("password", password);
         
         if (memberType.equals("manager")) {
-            ManagerDao managerDao = session.getMapper(ManagerDao.class);
             return managerDao.findByEmailPassword(params);
             
         } else if (memberType.equals("student")) {
-            StudentDao studentDao = session.getMapper(StudentDao.class);
             return studentDao.findByEmailPassword(params);
             
         } else if (memberType.equals("teacher")) {
-            TeacherDao teacherDao = session.getMapper(TeacherDao.class);
             return teacherDao.findByEmailPassword(params);
             // session.getMapper() : 실행하는 시점에서 인터페이스를 구현한 Proxy 객체(패턴)이 생김. 이것을 리턴함.
             // 여기서 규칙! 인터페이스와 같은 xml과 메서드 이름과 파라미터 타입이 일치한 메서드를 찾음. 
@@ -46,7 +41,6 @@ public class AuthServiceImpl implements AuthService {
         }
     }
     }
-}
 
 
 
--- java110-project/src/main/java/bitcamp/java110/cms/service/impl/TeacherServiceImpl.java
+++ java110-project/src/main/java/bitcamp/java110/cms/service/impl/TeacherServiceImpl.java
@@ -3,8 +3,6 @@ package bitcamp.java110.cms.service.impl;
 import java.util.HashMap;
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,17 +15,13 @@ import bitcamp.java110.cms.service.TeacherService;
 @Service
 public class TeacherServiceImpl implements TeacherService {
 
-    @Autowired
-    SqlSessionFactory sqlSessionFactory;
+    @Autowired TeacherDao teacherDao;
+    @Autowired MemberDao memberDao;
+    @Autowired PhotoDao photoDao;
+
 
     @Override
     public void add(Teacher teacher) {
-        SqlSession session = sqlSessionFactory.openSession();
-        try{
-            MemberDao memberDao = session.getMapper(MemberDao.class);
-            TeacherDao teacherDao = session.getMapper(TeacherDao.class);
-            PhotoDao photoDao = session.getMapper(PhotoDao.class);
-
             memberDao.insert(teacher);
             teacherDao.insert(teacher);
 
@@ -37,58 +31,30 @@ public class TeacherServiceImpl implements TeacherService {
                 params.put("photo", teacher.getPhoto());
                 photoDao.insert(params);
             }
-            session.commit();
-        }catch(Exception e) {
-            session.rollback();
-            throw e;
-        }finally {
-            session.close();
-        }
-
     }
 
     @Override
     public List<Teacher> list(int pageNo, int pageSize) {
-        try(SqlSession session = sqlSessionFactory.openSession()){
-
-            TeacherDao teacherDao = session.getMapper(TeacherDao.class);
             HashMap<String, Object> params = new HashMap<>();
             params.put("rowNo", (pageNo-1)*pageSize);
             params.put("size", pageSize);
 
             return teacherDao.findAll(params);
-        }
     }
 
     @Override
     public Teacher get(int no) {
-        try(SqlSession session = sqlSessionFactory.openSession()){
-
-            TeacherDao teacherDao = session.getMapper(TeacherDao.class);
             return teacherDao.findByNo(no);
-        }
+        
     }
     
     @Override
     public void delete(int no) {
-        SqlSession session = sqlSessionFactory.openSession();
-        try{
-            MemberDao memberDao = session.getMapper(MemberDao.class);
-            TeacherDao teacherDao = session.getMapper(TeacherDao.class);
-            PhotoDao photoDao = session.getMapper(PhotoDao.class);
-
-            if (teacherDao.delete(no) == 0) {
+            if (teacherDao.delete(no) == 0) 
                 throw new RuntimeException("해당 번호의 데이터가 없습니다.");
-            }
+            
             photoDao.delete(no);
             memberDao.delete(no);
-            session.commit();
-        }catch(Exception e) {
-            session.rollback();
-            throw e;
-        }finally {
-            session.close();
-        }
     }
 }
 
