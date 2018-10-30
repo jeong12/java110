729cfb58a22618763146fdd2d4114cbc98e09b6c
Jeong <hee2012j@naver.com>
2018-10-30 11:47:50 +0900

    Spring AOP AppConfig로 설정

---
+++ java110-spring-ioc/src/main/java/ex11/step7/Service.java
@@ -0,0 +1,9 @@
+package ex11.step7;
+
+public interface Service {
+    void add();
+    void update();
+    void delete();
+    void list();
+    int addPhoto(String filename);
+}
---
+++ java110-spring-ioc/src/main/java/ex11/step8/Service.java
@@ -0,0 +1,9 @@
+package ex11.step8;
+
+public interface Service {
+    void add();
+    void update();
+    void delete();
+    void list();
+    int addPhoto(String filename);
+}
